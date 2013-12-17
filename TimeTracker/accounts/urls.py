eff1ce0ba83c673aaf1fec5939ee99a1878b5d83
NathanDalley <nathan.dalley.htc@gmail.com>
2013-12-17 11:11:19 +0000

    Testing

--- TimeTracker/accounts/urls.py
+++ TimeTracker/accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import views
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="accounts/index.html")),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^/login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page':'/time/'}, name='logout'),
     #url(r'^register/', 'django.contrib.auth.partials.register', name='register'),
 #    url(r'^login/$', partials.loginView.as_view(), name='login'),
